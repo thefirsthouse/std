fc072cc6225e95ce6daca35cd3385057e869708f
thefirsthouse <vasilijanenko4@gmail.com>
2025-03-26 14:22:51 +0300

    first commit

---
+++ lab_1/optimised.py
@@ -0,0 +1,12 @@
+def find_best_partition(N):
+    best_d = 1
+    for d in range(2, int(N**0.5) + 1):  # проверяем делители до корня
+        if N % d == 0:
+            best_d = max(best_d, d, N // d)  # берем максимальный возможный делитель
+    
+    A = best_d
+    B = N - best_d
+    print(A, B)
+
+N = int(input())
+find_best_partition(N)
